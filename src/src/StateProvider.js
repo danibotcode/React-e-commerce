96661490fe6ee73c4f0faf3f04b122a2801eadaa
danibotcode <45088418+danibotcode@users.noreply.github.com>
2020-09-30 20:06:53 -0700

    Update 3
    Completed shopping basket functionality

---
+++ src/src/StateProvider.js
@@ -0,0 +1,16 @@
+// setup data layer
+// track record of basket, to use at checkout page; track user for use at homepage
+import React, { createContext, useContext, useReducer} from "react";
+
+// Data layer
+export const StateContext = createContext();
+
+// Build provider
+export const StateProvider = ({reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+    </StateContext.Provider>
+);
+
+//Use inside of component
+export const useStateValue = () => useContext(StateContext);
